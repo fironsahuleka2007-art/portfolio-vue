0dcce6299bb505341fad148941feb52048e2d7d5
fironsahuleka2007-art <fironsahuleka2007@gmail.com>
2026-02-16 14:56:08 +0700

    update UI and layout

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -8,6 +8,7 @@ import Navbar from "./Navbar.vue"
   <section class="min-h-screen flex items-center">
     <div class="max-w-6xl mx-auto px-6 pt-24">
       <p class="text-sm text-gray-500 mb-3">
+        <span class="text-yellow-400 font-semibold">Highlight</span>
         Halo, saya Firon Fariz Sahuleka, Mahasiswa Teknik Informatika Politeknik Negeri Bandung.
       </p>
 
@@ -21,17 +22,13 @@ import Navbar from "./Navbar.vue"
       <p class="mt-6 max-w-xl text-gray-600">
         Dolor ipsum dolor sit amet, consectetur adipiscing elit. Donec vel mauris quam. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.
       </p>
-
+      <body class="bg-slate-50 text-slate-900"></body>
       <div class="mt-8 flex gap-4">
-        <button
-          class="px-6 py-3 bg-black text-white rounded-lg hover:bg-gray-800 transition"
-        >
+      <button class="bg-blue-900 text-white px-5 py-2 rounded-xl hover:bg-blue-800 transition">
           View Projects
-        </button>
+      </button>
 
-        <button
-          class="px-6 py-3 border border-gray-300 rounded-lg hover:bg-gray-100 transition"
-        >
+        <button class="bg-blue-900 text-white px-5 py-2 rounded-xl hover:bg-blue-800 transition">
           Contact Me
         </button>
       </div>
