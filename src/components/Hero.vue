4fb4ee757439290f874fdf5cd7dc90fd967337b6
fironsahuleka2007-art <fironsahuleka2007@gmail.com>
2026-02-16 14:38:32 +0700

    update header

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,16 +1,40 @@
+<script setup>
+import Navbar from "./components/Navbar.vue"
+</script>
+
 <template>
-  <section class="min-h-screen flex items-center justify-center pt-24">
-    <div class="text-center max-w-xl">
-      <h1 class="text-4xl md:text-5xl font-bold mb-4">
-        Hi, I'm <span class="text-blue-500">Firon</span>
-      </h1>
-      <p class="text-gray-600 mb-6">
-        SLEBEWW KEUNN WAEEE.
+  <Navbar />
+
+  <section class="min-h-screen flex items-center">
+    <div class="max-w-6xl mx-auto px-6 pt-24">
+      <p class="text-sm text-gray-500 mb-3">
+        Halo, saya Firon Fariz Sahuleka, Mahasiswa Teknik Informatika Politeknik Negeri Bandung.
       </p>
-      <a href="#projects"
-         class="inline-block px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
-        Lihat Project
-      </a>
+
+      <h1 class="text-4xl md:text-6xl font-bold leading-tight">
+        Firon Fariz Sahuleka.<br />
+        <span class="text-gray-400">
+          I build web interfaces.
+        </span>
+      </h1>
+
+      <p class="mt-6 max-w-xl text-gray-600">
+        Dolor ipsum dolor sit amet, consectetur adipiscing elit. Donec vel mauris quam. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.
+      </p>
+
+      <div class="mt-8 flex gap-4">
+        <button
+          class="px-6 py-3 bg-black text-white rounded-lg hover:bg-gray-800 transition"
+        >
+          View Projects
+        </button>
+
+        <button
+          class="px-6 py-3 border border-gray-300 rounded-lg hover:bg-gray-100 transition"
+        >
+          Contact Me
+        </button>
+      </div>
     </div>
   </section>
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,14 @@
 <template>
-  <nav class="fixed top-0 w-full z-50 bg-white/80 backdrop-blur border-b">
+    <nav class="fixed top-0 left-0 w-full bg-white/80 backdrop-blur border-b">
     <div class="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
       <h1 class="font-bold text-lg">Firon</h1>
-      <div class="space-x-6 text-sm">
-        <a href="#about" class="hover:text-blue-500 transition">About</a>
-        <a href="#projects" class="hover:text-blue-500 transition">Projects</a>
-        <a href="#contact" class="hover:text-blue-500 transition">Contact</a>
-      </div>
+
+         <ul class="flex gap-6 text-sm text-gray-600">
+        <li class="hover:text-black cursor-pointer">Home</li>
+        <li class="hover:text-black cursor-pointer">About</li>
+        <li class="hover:text-black cursor-pointer">Projects</li>
+        <li class="hover:text-black cursor-pointer">Contact</li>
+         </ul>
     </div>
   </nav>
 </template>
